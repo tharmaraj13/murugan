4d6a9e9a1ec159a1350031f3ce1f5ad37d18e68b
Tharmaraj N <tharmaraj@uitoux.in>
2025-03-01 12:25:13 +0530

    Update auth-guard.guard.ts

--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
 import { ApiserviceService } from './apiservice.service';
 
 @Injectable({
@@ -7,32 +7,33 @@ import { ApiserviceService } from './apiservice.service';
 })
 export class AuthGuardGuard {
   constructor(private apiservice: ApiserviceService, private router: Router) { }
-  async canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot) {
+  async canActivate(route: ActivatedRouteSnapshot) {
     const url: any = route.url[0]?.path;
-    const url1: any = route.routeConfig?.path;
     let data = this.apiservice.userData;
-    if (Object.keys(this.apiservice.userData).length === 0 || this.apiservice.userData.status != 'ok') {
+    if (Object.keys(data).length === 0 || data.status != 'ok') {
       data = this.apiservice.checking();
       this.apiservice.userData = data;
     }
-    if (data.status == 'ok') {
-      const permissions = JSON.parse(data.permissions);
-      if (url1 == '' || url == 'login') {
-        this.router.navigate([data.homepage]);
-      }
-      else if (!permissions[url]) {
-        this.router.navigate(['404-page']);
-      }
-      return true;
-    } else {
-      if (url1 == '' || url == 'login') {
-        return true;
-      }
+    if (data.status !== 'ok') {
+      if (url === 'login' || !route.routeConfig?.path) return true;
+      this.router.navigate(['login']);
+      return false;
     }
-    this.router.navigate(['login']);
-    return false;
+    const permissions = JSON.parse(data.permissions || '{}');
+
+    // If user tries to access login page while logged in, redirect to homepage
+    if (url === 'login' || !route.routeConfig?.path) {
+      this.router.navigate([data.homepage]);
+      return false;
+    }
+
+    // Check if the user has permission for this route
+    if (!permissions[url]) {
+      this.router.navigate(['404-page']);
+      return false;
+    }
+
+    return true;
   }
 
 }
